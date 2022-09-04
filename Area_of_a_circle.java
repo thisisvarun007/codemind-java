2f22bb15911da149de78351b24cd69f0680eee59
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-04 08:57:33 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        int a;        a=sc.nextInt();        double ar=3.14*a*a;        System.out.format("%.2f",ar);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a;
+       a=sc.nextInt();
+       double ar=3.14*a*a;
+       System.out.format("%.2f",ar);
+    }
+}
