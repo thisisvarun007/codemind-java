b84eefa57182299786d5ad34d1d0cfcc682faed2
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:40:05 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        Long a,b,c;        a=sc.nextLong();        b=sc.nextLong();        c=a+b;        System.out.print(c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       Long a,b,c;
+       a=sc.nextLong();
+       b=sc.nextLong();
+       c=a+b;
+       System.out.print(c);
+    }
+}
