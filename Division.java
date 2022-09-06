b5161aac8417ff7b8662560ef10b2e1da3885a33
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:28:46 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        int a,b;        a=sc.nextInt();        b=sc.nextInt();        int res=a/b;        System.out.format("%d",res);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a,b;
+       a=sc.nextInt();
+       b=sc.nextInt();
+       int res=a/b;
+       System.out.format("%d",res);
+    }
+}
