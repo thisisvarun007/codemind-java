3a21eda0b87ac45c6895830f0b59d484902df1a4
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:29:53 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        int a,b;        a=sc.nextInt();        b=sc.nextInt();        int res=a%b;        System.out.format("%d",res);     } }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a,b;
+       a=sc.nextInt();
+       b=sc.nextInt();
+       int res=a%b;
+       System.out.format("%d",res);
+    }
+}
