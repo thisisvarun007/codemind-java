c273899705bf54fd9df876e81652c9c47d9511c0
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:16:58 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        int a,b,c;        a=sc.nextInt();        b=sc.nextInt();        double avg=(a+b)*0.5;        System.out.format("%.4f",avg);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a,b,c;
+       a=sc.nextInt();
+       b=sc.nextInt();
+       double avg=(a+b)*0.5;
+       System.out.format("%.4f",avg);
+    }
+}
