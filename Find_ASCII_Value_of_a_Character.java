52e1df8fe8f298979a08175e3f19d74478ac074f
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:27:14 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        char c=sc.next().charAt(0);        int res=c;        System.out.format("%d",res);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       char c=sc.next().charAt(0);
+       int res=c;
+       System.out.format("%d",res);
+    }
+}
