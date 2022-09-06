a7cc596e719a772f77f7a2931dd19f1a2b55b19e
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-06 06:33:15 +0530

    import java.util.*; import java.lang.Math; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);        String str=sc.nextLine();        System.out.print(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       String str=sc.nextLine();
+       System.out.print(str);
+    }
+}
