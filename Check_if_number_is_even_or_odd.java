bf96455cc1b7b2d6060ab481d31fa797c6aad159
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-07 20:39:37 +0530

    import java.util.*; class solution {     public static void main(String agrs[])     {         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         if (a%2==0){             System.out.print("Even");         }         else{             System.out.print("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class solution
+{
+    public static void main(String agrs[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        if (a%2==0){
+            System.out.print("Even");
+        }
+        else{
+            System.out.print("Odd");
+        }
+    }
+}
