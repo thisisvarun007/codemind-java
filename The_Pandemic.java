a7d50064bfe515723ffe442b06fca219c2aed071
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-07 11:16:59 +0530

    import java.util.*; class solution {     public static void main(String agrs[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if (a<=1){             System.out.print("0");         }         else if (a==2){             System.out.print("1");         }         else{             System.out.print("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class solution
+{
+    public static void main(String agrs[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if (a<=1){
+            System.out.print("0");
+        }
+        else if (a==2){
+            System.out.print("1");
+        }
+        else{
+            System.out.print("2");
+        }
+    }
+}
