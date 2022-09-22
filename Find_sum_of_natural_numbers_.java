4c7436ab7105a598e0ceaeb6b87ee4592cf051a2
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-09-22 19:38:19 +0530

    import java.util.*; class solution{     public static void main(String agrs[]){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int sum = a*(a+1)/2;         System.out.print(sum);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class solution{
+    public static void main(String agrs[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = a*(a+1)/2;
+        System.out.print(sum);
+    }
+}
